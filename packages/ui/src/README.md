29583643c7855502adfb1663ba8e708342932167
Tetsuro43 <101857825+Tetsuro43@users.noreply.github.com>
2025-12-30 17:03:46 +0900

    fix: apply gitignore rules

--- packages/ui/src/README.md
+++
@@ -1,4 +0,0 @@
-## story book サーバ起動
-```bash
-npm run storybook
-```
--- packages/ui/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// StorybookとVitestの両方がこの設定を利用する
-export default defineConfig({
-  plugins: [
-    react(),
-  ],
-});
